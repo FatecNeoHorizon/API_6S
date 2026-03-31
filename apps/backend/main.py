2ddf478b8c912c0e365ce0d322b6fa07930d9cbf
Alexandre Jonas <jonassouzafonseca@gmail.com>
2026-03-30 21:52:17 -0300

    feat: adding provisory energy losses endpoint (wip)
    ref: #27

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 
 from src.model import blogModel
 from src.control import blogProcedures
-from src.model import distributionIndicesModel
 from src.control import distributionIndicesProcedures
+from src.control import energyLossesTariffProcedures
 
 from contextlib import asynccontextmanager
 from src.etl.database import setup
@@ -61,3 +61,19 @@ async def get_dec_fec(agent_acronym: str | None = None, cnpj_number: str | None 
     
     returnThing = distributionIndicesProcedures.DistributionIndicesProcedures().getAll(cleaned_dict)
     return returnThing
+
+# @app.get("/get-energy-losses")
+# async def get_dec_fec(distributor: str | None = None, distributor_slug: str | None = None, state: str | None = None, 
+#                       uf : str | None = None, process_date: str | None = None):
+#     filterDict = {
+#         "distributor" : distributor,
+#         "distributor_slug" : distributor_slug,
+#         "state" : state,
+#         "uf" : uf,
+#         "process_date" : process_date,
+#     }
+
+#     cleaned_dict = {key: value for key, value in filterDict.items() if value is not None}
+    
+#     returnThing = energyLossesTariffProcedures.EnergyLossesTariffProcedures().getAll(cleaned_dict)
+#     return returnThing

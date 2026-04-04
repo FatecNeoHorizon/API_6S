b5dbc0b03ae48eb58d14e1c43c2df10a352f4b18
Alexandre Jonas <jonassouzafonseca@gmail.com>
2026-04-04 19:27:46 -0300

    fix: filtering trough max and min process_date for energy losses endpoint

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -7,8 +7,6 @@ from src.control import energy_losses_tariff_procedures
 from src.control import blogProcedures
 from src.model import blogModel
 
-from src.utils.clean_filter import clean_filter, remove_operators_fields
-
 from contextlib import asynccontextmanager
 from src.etl.database import setup
 
@@ -79,13 +77,14 @@ async def get_dec_fec(agent_acronym: str | None = None, cnpj_number: str | None 
 
 @app.get("/get-energy-losses")
 async def get_energy_losses(distributor: str | None = None, distributor_slug: str | None = None, state: str | None = None, 
-                      uf : str | None = None, process_date: str | None = None):
+                      uf : str | None = None, process_date_min: str | None = None, process_date_max: str | None = None):
     filterDict = {
         "distributor" : distributor,
         "distributor_slug" : distributor_slug,
         "state" : state,
         "uf" : uf,
-        "process_date" : process_date,
+        "process_date" : {"$gte" : process_date_min,
+                "$lte" : process_date_max},
     }
     
     returnThing = energy_losses_tariff_procedures.Energy_losses_tariff_procedures().getAll(filterDict)

5bc48f631e1922e90ec03505082012b72cc1de0a
Cesar Pelogia <142257303+cesarpelogia@users.noreply.github.com>
2026-04-15 17:45:54 -0300

    chore(burndown): refactor fetch project data method for pagination
    inserted paginated method
    
    #236

--- burndown/src/main/java/com/zeus/burndown/integration/github/GitHubProjectDataProvider.java
+++ burndown/src/main/java/com/zeus/burndown/integration/github/GitHubProjectDataProvider.java
@@ -1,11 +1,11 @@
 package com.zeus.burndown.integration.github;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import com.zeus.burndown.dto.response.GraphQLResponse;
 import com.zeus.burndown.integration.github.model.GitHubProjectQueryData;
 import com.zeus.burndown.service.ProjectDataProvider;
@@ -18,72 +18,101 @@ public class GitHubProjectDataProvider implements ProjectDataProvider {
     private final int projectNumber;
 
     public GitHubProjectDataProvider(
-                GitHubGraphQLClient client,
-                @Value("${github.org}") String org,
-                @Value("${github.project-number}") int projectNumber) {
+            GitHubGraphQLClient client,
+            @Value("${github.org}") String org,
+            @Value("${github.project-number}") int projectNumber) {
         this.client = client;
         this.org = org;
         this.projectNumber = projectNumber;
     }
 
-    @Override
-    public GitHubProjectQueryData fetchProjectData() {
-        String query = """
-            query($org: String!, $number: Int!, $first: Int!, $after: String) {
-              organization(login: $org) {
-                projectV2(number: $number) {
-                  items(first: $first, after: $after) {
-                    pageInfo {
-                      hasNextPage
-                      endCursor
+    private static final String QUERY = """
+        query($org: String!, $number: Int!, $first: Int!, $after: String) {
+          organization(login: $org) {
+            projectV2(number: $number) {
+              items(first: $first, after: $after) {
+                pageInfo {
+                  hasNextPage
+                  endCursor
+                }
+                nodes {
+                  content {
+                    __typename
+                    ... on Issue {
+                      createdAt
+                      closedAt
+                      state
+                      milestone {
+                        createdAt
+                        dueOn
+                      }
                     }
-                    nodes {
-                      content {
-                        __typename
-                        ... on Issue {
-                          createdAt
-                          closedAt
-                          state
-                          milestone {
-                            createdAt
-                            dueOn
-                          }
-                        }
-                      }
-                      estimativa: fieldValueByName(name: "Estimativa (h)") {
-                        __typename
-                        ... on ProjectV2ItemFieldNumberValue {
-                          number
-                        }
-                        ... on ProjectV2ItemFieldTextValue {
-                          text
-                        }
-                      }
+                  }
+                  estimativa: fieldValueByName(name: "Estimativa (h)") {
+                    __typename
+                    ... on ProjectV2ItemFieldNumberValue {
+                      number
+                    }
+                    ... on ProjectV2ItemFieldTextValue {
+                      text
                     }
                   }
                 }
               }
             }
-        """;
-
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("org", this.org);
-        variables.put("number", this.projectNumber);
-        variables.put("first", 50);
-        variables.put("after", null);
-
-        GraphQLResponse response = client.execute(query, variables);
-
-        if (response == null) {
-            throw new RuntimeException("GraphQL response is null");
+          }
         }
+    """;
 
+    @Override
+    public GitHubProjectQueryData fetchProjectData() {
+        String after = null;
+        boolean hasNextPage = true;
 
-        if (response.getData() == null) {
-            throw new RuntimeException("GraphQL returned no data");
-        }
+        // Acumula os nodes de todas as páginas
+        List<Object> allNodes = new ArrayList<>();
+        GitHubProjectQueryData finalData = null;
 
-        return response.getData();
+        do {
+            Map<String, Object> variables = new HashMap<>();
+            variables.put("org", this.org);
+            variables.put("number", this.projectNumber);
+            variables.put("first", 50);
+            variables.put("after", after);
+
+            GraphQLResponse response = client.execute(QUERY, variables);
+
+            if (response == null) {
+                throw new RuntimeException("GraphQL response is null");
+            }
+            if (response.getData() == null) {
+                throw new RuntimeException("GraphQL returned no data");
+            }
+
+            GitHubProjectQueryData pageData = response.getData();
+
+            if (finalData == null) {
+                finalData = pageData;
+            }
+
+            var items = pageData
+                    .getOrganization()
+                    .getProjectV2()
+                    .getItems();
+
+            allNodes.addAll(items.getNodes());
+
+            hasNextPage = items.getPageInfo().isHasNextPage();
+            after = items.getPageInfo().getEndCursor();
+
+        } while (hasNextPage);
+
+        finalData
+            .getOrganization()
+            .getProjectV2()
+            .getItems()
+            .setNodes(allNodes);
+
+        return finalData;
     }
-
 }

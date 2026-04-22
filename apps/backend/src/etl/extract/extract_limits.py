f02b6f1a9b8f59177fa3d7eb5eeab24efe098f0b
Alexandre Jonas <jonassouzafonseca@gmail.com>
2026-04-22 20:41:17 -0300

    refactor: dynamically searching for the limits .csv full path ref: #111

--- apps/backend/src/etl/extract/extract_limits.py
+++ apps/backend/src/etl/extract/extract_limits.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import os
 from pathlib import Path
+from src.utils.find_uploaded_file import find_file_full_path
 
 #The list returned can be retrieved with commands such as "data_dict[1]" or data_dict[1]['UF']
 #Only the 'DatGeracaoConjuntoDados' column won't be used
 def extract_limits():
-    path_value = os.getenv("LIMITS_FILE_PATH")
+
+    file_name = os.getenv("LIMITS_FILE_NAME")
+    search_folder = os.getenv("TMP_UPLOAD_PATH")
+
+    path_value = find_file_full_path(file_name, search_folder)
     
     if not path_value:
-        raise ValueError("LIMITS_FILE_PATH environment variable is not set.")
+        raise ValueError("LIMITS_FILE_NAME environment variable is not set.")
 
     path = Path(path_value)
 

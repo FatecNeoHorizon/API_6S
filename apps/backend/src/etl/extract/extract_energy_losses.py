cf4cd6b963bfe202ad708e54c34ff6bfe4f132df
Alexandre Jonas <jonassouzafonseca@gmail.com>
2026-04-22 20:34:30 -0300

    refactor: creating generic funtion to find full file path regardless of the file being extracted. Using this function on the extracting energy-losses routine

--- apps/backend/src/etl/extract/extract_energy_losses.py
+++ apps/backend/src/etl/extract/extract_energy_losses.py
@@ -1,19 +1,19 @@
 import os
 import pandas as pd
 from pathlib import Path
+from src.utils.find_uploaded_file import find_file_full_path
 
 #The list returned can be retrieved with commands such as "data_dict[1]" or data_dict[1]['UF']
 #All columns of the excel file will be used
 def extract_losses():
 
-    # path_value = os.getenv("LOSSES_FILE_PATH")
-    file_name = os.getenv("LOSSES_FILE_PATH")
+    file_name = os.getenv("LOSSES_FILE_NAME")
     search_folder = os.getenv("TMP_UPLOAD_PATH")
 
-    path_value = _find_file(file_name, search_folder)
+    path_value = find_file_full_path(file_name, search_folder)
     
     if not path_value:
-        raise ValueError("LOSSES_FILE_PATH environment variable is not set.")
+        raise ValueError("LOSSES_FILE_NAME environment variable is not set.")
 
     path = Path(path_value)
 
@@ -21,9 +21,3 @@ def extract_losses():
     data_dict = df.to_dict(orient='records')
 
     return data_dict
-
-def _find_file(file_name, search_folder):
-    for root, dirs, files in os.walk(search_folder):
-        if file_name in files:
-            return os.path.join(root, file_name)
-    return None

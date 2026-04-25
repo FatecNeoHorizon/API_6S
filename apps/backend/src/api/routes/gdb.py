920431ae8660d533dc74e91f7a769e704e3536a0
gabriel15asouza <gabalves.souza@gmail.com>
2026-04-25 02:34:38 -0300

    feat(backend): set orchestrator to manage extraction proccess

--- apps/backend/src/api/routes/gdb.py
+++ apps/backend/src/api/routes/gdb.py
@@ -1,10 +1,11 @@
+from src.etl.extract.gdb_orchestrator import run_extraction
 from fastapi import APIRouter, Depends
 from pymongo.database import Database
-from src.database.connection import get_db  # ajuste conforme seu projeto
-from src.etl.extract import extract_gdb
+from src.database.connection import get_db
+from src.etl.extract.gdb_orchestrator import run_extraction
 
 router = APIRouter()
 
 @router.get("/test-geodatabase-extraction")
 async def test_geodatabase_extraction(db: Database = Depends(get_db)):
-    return extract_gdb.extract_gdb(db)
+    return run_extraction(db)
---
+++ apps/backend/src/etl/extract/gdb_extractor.py
@@ -0,0 +1,57 @@
+import logging
+import pyogrio as pyo
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def extract_gdb_generator(path: Path, chunk_size: int = 100):
+    raw_layers = pyo.list_layers(path)
+
+    target_layers = {
+        "CONJ",
+        "SUB",
+        "UNTRMT",
+        "UNTRAT",
+    }
+
+    layer_names = [
+        layer[0]
+        for layer in raw_layers
+        if layer[0] in target_layers
+    ]
+
+    for layer_name in layer_names:
+        logger.info(f"[START] Lendo layer: {layer_name}")
+
+        try:
+            gdf = pyo.read_dataframe(path, layer=layer_name)
+
+            logger.info(f"[LOADED] {layer_name} - {len(gdf)} registros")
+
+            if gdf.crs is None:
+                raise ValueError(f"Layer {layer_name} sem CRS")
+
+            if gdf.crs.to_epsg() != 4326:
+                logger.info(f"[CRS] Convertendo {layer_name}")
+                gdf = gdf.to_crs(epsg=4326)
+
+            if "geometry" in gdf.columns:
+                logger.info(f"[GEOM] Serializando {layer_name}")
+                gdf["geometry"] = gdf["geometry"].apply(
+                    lambda g: g.__geo_interface__ if g else None
+                )
+
+            for i in range(0, len(gdf), chunk_size):
+                logger.info(f"[CHUNK] {layer_name} - {i}")
+
+                yield gdf.iloc[i:i+chunk_size], layer_name, str(path)
+
+        except Exception as e:
+            logger.error(f"[ERROR] {layer_name}: {str(e)}")
+
+            yield {
+                "error": str(e),
+                "layer_name": layer_name,
+                "source_file": str(path)
+            }, layer_name, str(path)
